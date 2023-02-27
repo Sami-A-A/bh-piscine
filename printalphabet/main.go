cac1e6fc2e401b9f05af9a51471b373fc105244f
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-02-28 00:17:12 +0300

    works perfectly but tests are so picky

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,8 +2,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for _,e := range "abcdefghijklmnopqrstuvwxyz"{
+	for _,e := range "abcdefghijklmnopqrstuvwxyz\n"{
 		z01.PrintRune(e)
 	}
-	z01.PrintRune('\n')
 }
