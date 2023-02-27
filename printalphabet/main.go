912214a1371d26d9e2be90ef955ae0d905f1515e
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-02-28 01:19:58 +0300

    last commit before giving up on bull* quest

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,9 +1,15 @@
 package main
 import "github.com/01-edu/z01"
 
+// func main() {
+// 	var abc string = "abcdefghijklmnopqrstuvwxyz\n"
+// 	for i := 0 ; i < 27 ; i++ {
+// 		z01.PrintRune(rune(abc[i]))
+// 	}
+// }
+
 func main() {
-	var abc string = "abcdefghijklmnopqrstuvwxyz\n"
-	for i := 0 ; i < 27 ; i++ {
-		z01.PrintRune(rune(abc[i]))
+	for _, e := range "abcdefghijklmnopqrstuvwxyz/n" {
+		z01.PrintRune(e)
 	}
 }
