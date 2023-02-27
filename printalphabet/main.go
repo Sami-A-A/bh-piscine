1af75895d7dd850eab737b356cb77cca148f5057
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-02-28 00:52:38 +0300

    absolutely ridiculous exercise

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,8 +2,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var abc string = "abcdefghijklmnopqrstuvwxyz\n"
-	for i := 0 ; i < 27 ; i++ {
+	var abc string = "abcdefghijklmnopqrstuvwxyz"
+	for i := 0 ; i < 26 ; i++ {
 		z01.PrintRune(rune(abc[i]))
 	}
 }
