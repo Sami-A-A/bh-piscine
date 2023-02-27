3962149d100506491643b4654935021ed6de5212
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-02-28 01:43:40 +0300

    q02l02a02.2

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -2,9 +2,9 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main(){
-	
-	for _, e := range "zyxwvutsrqponmlkjihgfedcba"{
+func main() {
+
+	for _, e := range "zyxwvutsrqponmlkjihgfedcba" {
 		z01.PrintRune(e)
 	}
 	z01.PrintRune('\n')
