ca798849dc84c807c28a2f44056e5b6471e303f5
sabdulaa <sami.abdulaal@gmail.com>
2023-02-27 12:03:18 +0300

    attempt 6

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
-export HERO_ID=70
+# ! /bin/bash
+# export HERO_ID=70
 curl -s https://learn.reboot01.com/assets/superhero/all.json | jq ".[] | select(.id==$HERO_ID)" | jq ".connections.relatives" | tr -d '"'
