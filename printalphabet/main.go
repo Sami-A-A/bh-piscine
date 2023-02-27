c2b423655041e6d70a52bc731a3e96165c07d495
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-02-28 00:51:39 +0300

    attempt 999 for impossible tests

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,7 +2,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for _, e := range "abcdefghijklmnopqrstuvwxyz\n"{
-		z01.PrintRune(e)
+	var abc string = "abcdefghijklmnopqrstuvwxyz\n"
+	for i := 0 ; i < 27 ; i++ {
+		z01.PrintRune(rune(abc[i]))
 	}
 }
