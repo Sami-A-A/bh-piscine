cf763cbd0fdf065dfc570108c4356346b02bf1db
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-02-28 00:46:11 +0300

    impossible task

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,7 +2,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for _,e := range "abcdefghijklmnopqrstuvwxyz\n"{
+	for _, e := range "abcdefghijklmnopqrstuvwxyz\n"{
 		z01.PrintRune(e)
 	}
 }
