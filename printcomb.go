68c2ff4d580756d07f8dd271c83c6aceb4394b81
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-02-28 03:22:56 +0300

    q02l05a04

--- printcomb.go
+++ printcomb.go
@@ -9,7 +9,7 @@ func PrintComb() {
 	for a := 0; a <= 9; a++ {
 		for b := 0; b <= 9; b++ {
 			for c := 0; c <= 9; c++ {
-				if digits[a] != digits[b] && digits[a] != digits[c] && digits[b] != digits[c] {
+				if digits[a] < digits[b] && digits[b] < digits[c] {
 					z01.PrintRune(rune(digits[a]))
 					z01.PrintRune(rune(digits[b]))
 					z01.PrintRune(rune(digits[c]))
