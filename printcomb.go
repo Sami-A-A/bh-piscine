5041ee66aed6ac907a490c07bbb215bb321359f0
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-02-28 03:28:07 +0300

    ??

--- printcomb.go
+++ printcomb.go
@@ -3,9 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-
 	var digits = "0123456789"
-
 	for a := 0; a <= 9; a++ {
 		for b := 0; b <= 9; b++ {
 			for c := 0; c <= 9; c++ {
