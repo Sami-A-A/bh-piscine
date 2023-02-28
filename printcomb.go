7fc4111990b79a93ab2925c5b39e39a9f1de51b6
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-02-28 03:17:08 +0300

    q02l5a2

---
+++ printcomb.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+
+	var digits = "0123456789"
+
+	for a := 0; a <= 9; a++ {
+		for b := 0; b <= 9; b++ {
+			for c := 0; c <= 9; c++ {
+				if digits[a] != digits[b] && digits[a] != digits[c] && digits[b] != digits[c] {
+					z01.PrintRune(rune(digits[a]))
+					z01.PrintRune(rune(digits[b]))
+					z01.PrintRune(rune(digits[c]))
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+}
--- printdigits/main.go
+++ printdigits/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+	"piscine"
+)
 
 func main() {
 	for _, e := range "0123456789" {
 		z01.PrintRune(e)
 	}
 	z01.PrintRune('\n')
+
+	piscine.PrintComb()
 }
