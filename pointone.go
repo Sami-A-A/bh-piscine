b682d560a6c04949284902874b7ee45edb76423d
reboot <student@desk-0120.reboot.internal>
2023-02-28 14:07:55 +0300

    q01l9a09

---
+++ pointone.go
@@ -0,0 +1,9 @@
+package main
+
+func PointOne(n *int){
+	n* = 1
+}
+
+func main(){
+
+}
--- printcomb.go
+++ printcomb.go
@@ -4,10 +4,11 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	digits := "0123456789"
+	fmt.Println(digits[a])
 	for a := 0; a <= 9; a++ {
 		for b := 0; b <= 9; b++ {
 			for c := 0; c <= 9; c++ {
-				if digits[a] < digits[b] && digits[b] < digits[c] {
+				if a < b && b < c {
 					z01.PrintRune(rune(digits[a]))
 					z01.PrintRune(rune(digits[b]))
 					z01.PrintRune(rune(digits[c]))
