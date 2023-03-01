ec825b2bb3ea1fce1e7b4e5a94bf7612d719f8e9
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-01 04:52:21 +0300

    dumb time 2

--- skip.sh
+++ skip.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-ls -1 | awk -v now="$(date +"%b 1 %H:%M ")" '{ if(NR%2!=0){ print "-rw-r--r-- 1 1000 1000 0 " now  $0}}' 
+ls -1 | awk -v now="$(date +"%b  1 %H:%M ")" '{ if(NR%2!=0){ print "-rw-r--r-- 1 1000 1000 0 " now  $0}}' 
