21813ac0e0421f972604840ffbc563092642f5ce
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-01 03:26:53 +0300

    q02l06a03

--- printcomb2.go
+++ printcomb2.go
@@ -9,13 +9,13 @@ func PrintComb2() {
 		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
 				for d := '0'; d <= '9'; d++ {
-					if a+b != c+d && a+b < c+d {
+					if a*10+b < c*10+d {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
 						z01.PrintRune(c)
 						z01.PrintRune(d)
-						if a == '9' && b == '9' && c == '9' && d == '8' {
+						if a == '9' && b == '8' && c == '9' && d == '9' {
 							z01.PrintRune('\n')
 						} else {
 							z01.PrintRune(',')
