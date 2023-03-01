c26fe6d5501aa0d7ba486a4ece93c30d6692c0b0
reboot <student@desk-0120.reboot.internal>
2023-03-01 13:31:31 +0300

    q03l04a01

--- divmod.go
+++ divmod.go
@@ -1,8 +1,8 @@
 package piscine
 
 func DivMod(a int, b int, div *int, mod *int) {
-	dividend := a / b
-	remainder := a % b
+	dividend := a/b
+	remainder := a%b
 	*div = dividend
 	*mod = remainder
 }
