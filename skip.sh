2f3e1b82e641b4f93aee1b259bc4d0527b357fba
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-01 04:48:07 +0300

    q01l??a??

--- skip.sh
+++ skip.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-ls -1 | awk '{ if(NR%2!=0){ print "-rw-r--r-- 1 1000 1000 0" strftime(" %b %d %H:%M ", systime()) $0 } }'
+ls -1 | awk -v now="$(date +"%b %d %H:%M ")" '{ if(NR%2!=0){ print "-rw-r--r-- 1 1000 1000 0 " now  $0}}' 
