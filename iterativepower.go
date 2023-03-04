d0432de197d08ee80990c5c953890e98ecb7cbf2
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-04 22:05:11 +0300

    q04l03a02

--- iterativepower.go
+++ iterativepower.go
@@ -1,9 +1,16 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
+	if nb < 0 {
+		return 0
+	}
 	a := nb
 	for i := 1; i < power; i++ {
 		nb *= a
 	}
-	return nb
+	if nb < 0 {
+		return 0
+	} else {
+		return nb
+	}
 }
