bf2559e7ff79d2a4b63c24546f54af6ddc0b0737
sabdulaa <git@github.com>
2023-03-05 15:52:20 +0300

    q05l03a02

--- nrune.go
+++ nrune.go
@@ -2,8 +2,8 @@ package piscine
 
 func NRune(s string, n int) rune {
 	a := []rune(s)
-	if n > len(a)-1 && n > 0 {
-		a[n]
+	if n-1 < len(a) && n >= 0 {
+		return a[n-1]
 	} else {
 		return '0'
 	}
