0d282a9ce59200b7d76ca1a4362669023c34668e
sabdulaa <git@github.com>
2023-03-05 15:18:00 +0300

    something

--- test/test.go
+++ test/test.go
@@ -1,13 +1,13 @@
-package main
+// package main
 
-import (
-	"github.com/01-edu/z01"
-	"piscine"
-)
+// import (
+// 	"github.com/01-edu/z01"
+// 	"piscine"
+// )
 
-func main() {
-	z01.PrintRune(piscine.LastRune("Hello!"))
-	z01.PrintRune(piscine.LastRune("Salut!"))
-	z01.PrintRune(piscine.LastRune("Ola!"))
-	z01.PrintRune('\n')
-}
+// func main() {
+// 	z01.PrintRune(piscine.LastRune("Hello!"))
+// 	z01.PrintRune(piscine.LastRune("Salut!"))
+// 	z01.PrintRune(piscine.LastRune("Ola!"))
+// 	z01.PrintRune('\n')
+// }
