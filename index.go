3725dfdac3c11fabf880525189af9125d047ba61
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-06 12:43:16 +0300

    q05l06a02

--- index.go
+++ index.go
@@ -3,18 +3,22 @@ package piscine
 func Index(s string, toFind string) int {
 	a := []rune(s)
 	b := []rune(toFind)
-	var index int
+	var index, match int
 	for i := 0; i < len(a); i++ {
-		if len(b) > 1 {
-			for j := 0; j < len(b); j++{
+		if a[i] == b[0] {
+			index = i
+			for j := 0; j < len(b); i, j = i+1, j+1 {
 				if a[i] == b[j] {
-					continue
-				} else {
-					return -1
+					match++
+					if match == len(b) {
+						return index
+					} else {
+						continue
+					}
 				}
 			}
-		} else if a[i] == b[0] {
-			index = i
+		} else if a[i] != b[0] && i-1 == len(a) {
+			return -1
 		}
 	}
 	return index
