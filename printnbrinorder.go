eddbc55cc2d791b96451cac4da5662b01243c268
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-06 22:08:47 +0300

    test 009988766488899

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,17 +3,13 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
-	if n == 3159500242930794280 {
-		z01.PrintRune('0')
-		n /= 10
-	}
 	if n <= 0 {
 		z01.PrintRune('0')
 	}
 	var a []rune
 	for x := 1; n/x > 0; x *= 10 {
-		if x*10 > n {
-			for x := x; x*10 > n; x /= 10 {
+		if x*10 > n || x*10 < 0 {
+			for x := x; x*10 > n || x*10 < 0; x /= 10 {
 				a = append(a, rune(int(n/x)))
 				n -= int(n/x) * x
 				if n > 10 && n/(x/10) < 0 {
--- test/test.go
+++ test/test.go
@@ -6,4 +6,5 @@ func main() {
 	piscine.PrintNbrInOrder(321)
 	piscine.PrintNbrInOrder(0)
 	piscine.PrintNbrInOrder(3159500242930794280)
+	piscine.PrintNbrInOrder(8009509337222607481)
 }
