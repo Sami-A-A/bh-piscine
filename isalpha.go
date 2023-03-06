708e4dc2bbc54755de7c26ef6e0b33f0284f0c19
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-06 16:33:29 +0300

    q05l10a02

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, e := range s {
-		if (e < 'a' || e > 'z') && (e < 'A' || e > 'Z') {
+		if (e < 'a' || e > 'z') && (e < 'A' || e > 'Z') && (e < 48 || e > 57) {
 			return false
 		}
 	}
