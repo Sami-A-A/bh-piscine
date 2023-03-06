b874c3d3fb9546b36873dc0d651f1ee14b245f34
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-06 15:32:40 +0300

    asdfasdfasfasjdfoaijhfgiouawhnfgv iaefn

--- lastrune.go
+++ lastrune.go
@@ -1,6 +1,19 @@
 package piscine
 
-func LastRune(s string) rune {
-	a := []rune(s)
-	return a[len(a)-1]
+func ToUpper(s string) string {
+	alr := []rune(s)
+	res := []rune{}
+
+	for j := 0; j <= len(alr)-1; j++ {
+		for k := 'A'; k <= 'Z'; k++ {
+			if alr[j] >= 'a' && alr[j] <= 'z' {
+				res = append(res, alr[j]-32)
+				break
+			} else {
+				res = append(res, alr[j])
+				break
+			}
+		}
+	}
+	return string(res)
 }
