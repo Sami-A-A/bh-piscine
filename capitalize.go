4f3d47e2202538512b1ed292eca32161d8b9892b
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-07 13:39:17 +0300

    q05l16a01

---
+++ capitalize.go
@@ -0,0 +1,20 @@
+package piscine
+
+func Capitalize(s string) string {
+	a := []rune(s)
+	initial := true
+	for i, e := range a {
+		if (e >= 'a' && e <= 'z') && initial {
+			a[i] = e - 32
+			initial = false
+		} else if a[i] >= 'A' && a[i] <= 'Z' && initial {
+			initial = false
+		} else if a[i] >= 'A' && a[i] <= 'Z' && !initial {
+			a[i] = e + 32
+		} else if (a[i] < 'a' || a[i] > 'z') && (a[i] < 'A' || a[i] > 'Z') {
+			initial = true
+		}
+	}
+	s = string(a)
+	return s
+}
