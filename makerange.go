d466a78bcfcbd0c3f1e6875634e69dd536aa2983
sabdulaa <sami.abdulaal@gmail.com>
2023-03-08 17:27:04 +0300

    q07l02a02

--- makerange.go
+++ makerange.go
@@ -2,9 +2,10 @@ package piscine
 
 func MakeRange(min, max int) []int {
 	var a []int
+	var len int = max - min
 	if min < max {
-		b := make([]int, max-min)
-		for i := 0; i < len(b); i++ {
+		b := make([]int, len)
+		for i := 0; i < len; i++ {
 			b[i] = min + i
 		}
 		return b
