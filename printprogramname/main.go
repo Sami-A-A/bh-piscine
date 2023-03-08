96e808141afe3bb276f5c08f5c254d56c6af53f0
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-08 23:57:36 +0300

    q06l01a04

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -10,18 +10,16 @@ func PrintProgramName() {
 	dir := os.Args[0]
 	var filename string
 	for i := 0; i < len(dir); i++ {
-		if dir[i] == '/' {
+		if dir[i] == '/' || dir[i] == '\\' {
 			filename = ""
 			continue
-		} else if dir[i] == '.' && dir[i+1] != '/' || dir[i+1] != '.' {
+		} else if dir[i] == '.' && !(dir[i+1] == '/' || dir[i+1] == '\\' || dir[i+1] == '.') {
 			for _, e := range filename {
 				z01.PrintRune(e)
 			}
-			for _, e := range dir {
-				z01.PrintRune(e)
-			}
 			break
+		} else {
+			filename += string(dir[i])
 		}
-		filename += string(dir[i])
 	}
 }
