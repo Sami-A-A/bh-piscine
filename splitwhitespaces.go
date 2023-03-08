6c82e5d13c7bf789a0b11427d3b94951df6a52f8
sabdulaa <sami.abdulaal@gmail.com>
2023-03-08 18:28:47 +0300

    q07l04a02

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,10 +4,10 @@ func SplitWhiteSpaces(s string) []string {
 	var a []string
 	var b string
 	for i := 0; i < len(s); i++ {
-		if rune(s[i]) <= 32 {
+		if s[i] == ' ' || s[i] == '\n' || s[i] == '\t' {
 			a = append(a, b)
 			b = ""
-		} else if s[i] > 32 {
+		} else if !(s[i] == ' ' || s[i] == '\n' || s[i] == '\t') {
 			b += string(s[i])
 		}
 	}
