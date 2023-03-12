267852ff2704f682c4d75b5a276dbf7ecf903d5b
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-12 19:19:44 +0300

    q09l05a04

--- issorted.go
+++ issorted.go
@@ -1,8 +1,8 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := 0; i < len(a); i++ {
-		if f(a[i+1], a[i]) <= 0 {
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i+1], a[i]) < 0 {
 			return false
 		}
 	}
