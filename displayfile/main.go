6d87bbdc9cb4573ac74cc0c273f9dfa884f6d51d
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-12 16:27:45 +0300

    q08l03a01

---
+++ displayfile/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	// "io"
+	"fmt"
+)
+
+func main() {
+	if len(os.Args[1:]) > 1 {
+		fmt.Println("Too many arguments")
+		return
+	} else if len(os.Args[1:]) < 1 {
+		fmt.Println("File name missing")
+		return
+	}
+	file, err := os.Open("quest8.txt")
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+	}
+	arr := make([]byte, 14)
+	file.Read(arr)
+	fmt.Println(arr)
+	file.Close()
+}
