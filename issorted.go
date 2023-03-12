4901c02382447e50b5fa6be1fd814a61b34937c3
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-12 21:48:56 +0300

    q09l05a06

--- issorted.go
+++ issorted.go
@@ -1,8 +1,11 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	isSortedAscendingly := true
 	for i := 0; i < len(a)-1; i++ {
-		if f(a[i+1], a[i]) < 0 {
+		if f(a[i+1], a[i]) < 0 && isSortedAscendingly {
+			isSortedAscendingly = false
+		} else if f(a[i+1], a[i]) > 0 && !isSortedAscendingly {
 			return false
 		}
 	}
