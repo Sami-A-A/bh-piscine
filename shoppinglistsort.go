f711bd748a905466834341d6a6057ea2d63d6221
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-15 00:31:33 +0300

    teststesttes

--- shoppinglistsort.go
+++ shoppinglistsort.go
@@ -1,12 +1,12 @@
 package piscine
 
 func ShoppingListSort(slice []string) []string {
-	list := slice
-	for i, j := 1, 0; j < len(slice); i++ {
+	list := make([]string, len(slice))
+	for i, j := 1, 0; j < len(slice); i, j = i+1, j+1 {
 		for _, e := range slice {
 			if len(e) == i {
 				list[j] = e
-				j = j + 1
+				break
 			}
 		}
 	}
