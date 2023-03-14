1a80029d2af064dcb02466ed4593fbcc496d4fd1
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-14 21:20:19 +0300

    asdfasdfasdgagsdfhgs

---
+++ enigma.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	pa := ***a
+	pb := *b
+	pc := *******c
+	pd := ****d
+	***a = pc
+	*b = pa
+	*******c = pd
+	****d = pb
+}
