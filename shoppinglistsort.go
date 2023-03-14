19c3d24af28fda44df35e4dd1a9fb756ab7604b5
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-14 23:58:27 +0300

    asdfasdf

--- shoppinglistsort.go
+++ shoppinglistsort.go
@@ -1,11 +1,12 @@
 package piscine
 
 func ShoppingListSort(slice []string) []string {
-	list := []string{}
-	for i := 1; len(list) < len(slice); i++ {
+	list := slice
+	for i, j := 1, 0; j < len(slice); i++ {
 		for _, e := range slice {
 			if len(e) == i {
-				list = append(list, e)
+				list[j] = e
+				j = j + 1
 			}
 		}
 	}
