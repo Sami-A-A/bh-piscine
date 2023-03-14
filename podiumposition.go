90759c473c6d48b7983f074847915a027b435e7b
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-15 02:46:54 +0300

    asdeprfdkgir

---
+++ podiumposition.go
@@ -0,0 +1,9 @@
+package piscine
+
+func PodiumPosition(podium [][]string) [][]string {
+	podium[0][0] = "1st Place"
+	podium[1][0] = "2nd Place"
+	podium[2][0] = "3rd Place"
+	podium[3][0] = "4th Place"
+	return podium
+}
