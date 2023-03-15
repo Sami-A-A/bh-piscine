d52d5a969a126cf721d82ce10119827a079e3be0
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-03-15 05:28:40 +0300

    test

---
+++ fixthemain/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type Door struct {
+	state bool
+}
+
+// const OPEN = true
+// const CLOSE = false
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func OpenDoor(ptrDoor *Door) bool {
+	PrintStr("Door Opening...")
+	return ptrDoor.state
+
+}
+
+func CloseDoor(ptrDoor *Door) bool {
+	PrintStr("Door Closing...")
+	return !ptrDoor.state
+}
+
+func IsDoorOpen(ptrDoor *Door) bool {
+	PrintStr("is the Door opened ?")
+	return ptrDoor.state
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?")
+	return ptrDoor.state
+}
+
+func main() {
+	door := &Door{}
+
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	} else if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+}
